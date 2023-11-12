e2f21a886a1434fe70bfd06e901543e50b7b84f3
Tyuiu.RagimovaMA@outlook.com <Tyuiu.RagimovaMA@outlook.com>
2023-11-12 20:30:30 +0500

    Task4

---
+++ Tyuiu.RagimovaMA.Sprint4.Task4.V20.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.RagimovaMA.Sprint4.Task4.V20.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.RagimovaMA.Sprint4.Task4.V20.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("629c8100-c5d6-4d64-9452-15b38e239c94")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
